8400801b13a0efcac951974e9096b19745c06d26
jamieellen <jamieellen.w@gmail.com>
2023-10-09 14:54:40 -0700

    slight problem with await?

--- src/components/tests/AnimalForm.test.js
+++ src/components/tests/AnimalForm.test.js
@@ -1,7 +1,29 @@
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import React from "react";
 import AnimalForm from "../AnimalForm";
+import userEvent from "@testing-library/user-event";
 
 test('renders without errors', () => {
       render(<AnimalForm />)
+})
+
+test('all feilds filled + user submits = species in list', () => {
+      //ARRANGE
+      render(<AnimalForm />);
+      const species = 'feline';
+      //ACT
+      const speciesInput = screen.getByLabelText(/species:/i);
+      userEvent.type(speciesInput, species);
+      const ageInput = screen.getByLabelText(/age:/i);
+      userEvent.type(ageInput, '9');
+      const notesInput = screen.getByLabelText(/notes:/i);
+      userEvent.type(notesInput, 'Testing');
+      const submitButton = screen.getByRole('button');
+      userEvent.click(submitButton);
+      //ASSERT
+      await waitFor(() => {
+            const speciesFeedback = screen.queryByText(species);
+            expect(speciesFeedback).toBeInTheDocument();
+      })
+
 })
